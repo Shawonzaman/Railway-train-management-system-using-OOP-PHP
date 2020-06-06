5a6f7ba1c9083592b4afe9d4958a15e285fbfce4
Shawon Zaman <lonely.shawon@gmail.com>
2020-06-06 21:39:16 +0600

    Railway train management system

---
+++ footer.php
@@ -0,0 +1,25 @@
+<div class="footer">
+     	<div class="wrap">
+     	  <div class="footer-menu">
+     		<ul>
+				<li class="active"><a href="index.php">Home</a></li> 
+				<li><a href="http://shoaeb.com/">About RTMS</a></li> 
+				
+				<li><a href="http://shoaeb.com/">Privacy policy</a></li>
+		
+				<div class="clear"></div>
+			</ul>
+     	  </div>
+     	  <div class="footer-bottom">
+     	  	<div class="copy">
+			   <p>© Designed by <a href="http://google.com" target="_blank">Shawon</a></p>
+		    </div>
+		    <div class="social">	
+			 
+		    </div>
+		    <div class="clear"></div>
+     	  </div>
+       </div>
+     </div>
+</body>
+</html>
